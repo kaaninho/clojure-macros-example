aec43fc35ef7101984c5eaeb9df1efa3b77d8d06
Kaan Sahin <kaan.sahin@active-group.de>
2019-01-18 16:25:45 +0100

    Revised version

--- src/clojure_macros_example/core.clj
+++ src/clojure_macros_example/core.clj
@@ -1,5 +1,8 @@
 (ns clojure-macros-example.core)
 
+
+(println "\n\n----Warum DU Makros willst----\n")
+
 ;; Variante als Funktion
 (defn my-when1
   [pred then]
@@ -8,107 +11,105 @@
     nil))
 
 ;; Funktioniert, gibt im ersten Fall "Hallo Du", im zweiten nil zurück.
-(my-when1 true "Hallo Du")
-(my-when1 false "Hallo Du")
+(my-when1 (= 1 1) "Hallo Du")
+(my-when1 (= 2 1) "Hallo Du")
 
 ;; Funktioniert nicht, printet den String in die Konsole trotz 'false'
-(my-when1 false (println "Ich sollte nicht geprintet werden (Funktion)"))
+(my-when1 (= 2 1) (println "Ich sollte nicht geprintet werden (Funktion)"))
+
 
 ;; Variante als Makro
 (defmacro my-when
   [pred then]
-  (if pred
-    then
-    nil))
+  (list 'if pred
+        then
+        'nil))
 
 ;; Funktioniert nun auch wie erwartet, ohne Konsolenprint
-(my-when false (println "Ich sollte nicht geprintet werden (Makro)"))
+(my-when (= 2 1) (println "Ich sollte nicht geprintet werden (Makro)"))
 
 
 ;;; Code is Data, Data is Code
+(println "\n----Code is Data - Data is Code----\n")
 
-(println (+ 5 7))
+(println "1. " (+ 5 7))
 
-(println '(+ 5 7))
+(println "2. " '(+ 5 7))
 
-(println (eval '(+ 5 7)))
+(println "3. " (eval '(+ 5 7)))
 
-(println (list + 5 7))
+(println "4. " (list + 5 7))
 
-;; Kein String, sondern Symbol!
-(println (pr-str (quote ich-bin-ein-symbol)))
-(println (pr-str "ich-bin-ein-String"))
+(println "5. " (list '+ 5 7))
 
-;; Fehlermeldung
-;; (println (pr-str ich-bin-ein-symbol))
 
-(println (list '+ 5 7))
+;;; Makros
+(println "\n----Makros----\n")
 
+(println "Expandiertes my-when: " (macroexpand-1 '(my-when (= 1 1)
+                                                           "Hallo!")))
+
+(defmacro calc-infix
+  [form]
+  (list (second form) (first form) (nth form 2)))
+
+(println "Calc-Infix: " (calc-infix (2 + 3)))
 
 ;;; Das Record-Makro
+(println "\n----Das Record-Makro----\n")
 
 (defrecord Computer [cpu ram])
 (def office-pc (->Computer "AMD Athlon XP 1700 MHz" 2))
 (def gaming-pc (->Computer "Intel I5 6600 3600 MHz" 16))
 
-(println (str "\nDer Office-PC hat " (:ram office-pc) "GB RAM und einen "
-              (:cpu office-pc) " Prozessor.\n\n"))
+(println (str "Der Office-PC hat " (:ram office-pc) "GB RAM und einen "
+              (:cpu office-pc) " Prozessor.\n"))
 
 
-(defn def-my-record1
-  [type-name field]
-  (defn type-name [arg] {field arg}))
+;; Konstruktor eines Computers
+(defn ->>Computer
+  [ram-value]
+  {:ram ram-value})
+(->>Computer 3)
 
-(def-my-record1 'Computer2 'ram)
-
-;; Konstruktoraufruf schlaegt fehl
-;; (Computer2 2)
-;; Stattdessen die Funktion `type-name`
-(type-name 2)
-
-
-(defn def-my-record2
-  [type-name field]
-  (eval (list 'defn type-name '[arg] {field 'arg})))
-
-(def-my-record2 'Computer2 :ram)
-
-(Computer2 2)
 
 ;; 1. Versuch mit Makro
-(defmacro def-my-record3
+(defmacro def-my-record1
   [type-name field]
-  (list 'defn type-name '[arg] {(keyword field) 'arg}))
+  (list 'defn type-name ['arg] {(keyword field) 'arg}))
 
-(def-my-record3 Computer3 ram)
+(println "Expansion def-my-record1: " (macroexpand-1 '(def-my-record1 Computer1 ram)))
 
-(Computer3 2)
+(def-my-record1 Computer1 ram)
+(Computer1 2)
+(:ram (Computer1 2))
 
 
-(defmacro def-my-record4
+(defmacro def-my-record2
   [type-name field]
-  (list 'defn (symbol (str "make-" type-name)) '[arg] {(keyword field) 'arg}))
+  (list 'defn (symbol (str "->>" type-name)) '[arg] {(keyword field) 'arg}))
 
-(def-my-record4 Computer4 ram)
+(def-my-record2 Computer2 ram)
 
-(make-Computer4 2)
+(->>Computer2 2)
 
 
 ;; Version mit Praedikat
-(defmacro def-my-record5
+(defmacro def-my-record3
   [type-name field]
   (list 'do
-        (list 'defn (symbol (str "make-" type-name)) '[arg] {(keyword field) 'arg
-                                                             :__type__ (str type-name)})
+        (list 'defn (symbol (str "->>" type-name)) '[arg] {(keyword field) 'arg
+                                                           :__type__ (str type-name)})
         (list 'defn (symbol (str type-name "?")) '[el] (list '= (str type-name)
                                                              '(:__type__ el)))))
 
-(def-my-record5 Computer5 ram)
+(def-my-record3 Computer3 ram)
 
-(def my-office-pc (make-Computer5 2))
+(def my-office-pc (->>Computer3 2))
 (println "Mein Office-PC hat" (:ram my-office-pc) "GB RAM und ist tatsaechlich"
-         "vom Typ 'Computer5', denn: " (Computer5? my-office-pc))
+         "vom Typ 'Computer5', denn: " (Computer3? my-office-pc))
 
-(def-my-record5 Car color)
+(def-my-record3 Car color)
+(println "Ein Auto mit Farbe rot: " (->>Car "rot"))
 
-(println "Mein Office-PC hingegen ist nicht vom Typ 'Car', denn: " (Car? my-office-pc))
+(println "Mein Office-PC ist nicht vom Typ 'Car', denn: " (Car? my-office-pc))
