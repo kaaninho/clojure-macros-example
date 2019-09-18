6486955840a5edf4cf79f380c23705f6e383cc70
Kaan Sahin <kaan.sahin@active-group.de>
2019-09-18 16:56:35 +0200

    Add code for blog post 2. Add some code for blog post 3

--- src/clojure_macros_example/core.clj
+++ src/clojure_macros_example/core.clj
@@ -1,7 +1,7 @@
 (ns clojure-macros-example.core)
 
-
-(println "\n\n----Warum DU Makros willst----\n")
+(println "\n====Blogpost 1====")
+(println "\n----Warum DU Makros willst----\n")
 
 ;; Variante als Funktion
 (defn my-when1
@@ -17,6 +17,8 @@
 ;; Funktioniert nicht, printet den String in die Konsole trotz 'false'
 (my-when1 (= 2 1) (println "Ich sollte nicht geprintet werden (Funktion)"))
 
+;; Gibt die Liste (if true "Hallo" nil) zurück
+(list 'if true "Hallo" nil)
 
 ;; Variante als Makro
 (defmacro my-when
@@ -28,32 +30,136 @@
 ;; Funktioniert nun auch wie erwartet, ohne Konsolenprint
 (my-when (= 2 1) (println "Ich sollte nicht geprintet werden (Makro)"))
 
-
-;;; Code is Data, Data is Code
-(println "\n----Code is Data - Data is Code----\n")
-
-(println "1. " (+ 5 7))
-
-(println "2. " '(+ 5 7))
-
-(println "3. " (eval '(+ 5 7)))
-
-(println "4. " (list + 5 7))
-
-(println "5. " (list '+ 5 7))
-
-
-;;; Makros
-(println "\n----Makros----\n")
-
+;; Makro-Expansion
 (println "Expandiertes my-when: " (macroexpand-1 '(my-when (= 1 1)
                                                            "Hallo!")))
 
+;; Infix-Makro
 (defmacro calc-infix
   [form]
   (list (second form) (first form) (nth form 2)))
 
 (println "Calc-Infix: " (calc-infix (2 + 3)))
+
+
+;;;; Code zum zweiten Blogpost
+
+(println "\n\n====Blogpost 2====")
+
+(println "\n----Apostroph (Quote) und Syntax-Quote----\n")
+
+`(quote (+ 1 2))
+
+
+(defmacro my-when-mit-quote
+  [pred then]
+  '(if pred
+     then
+     nil))
+
+(macroexpand-1 '(my-when-mit-quote (= 1 1) "Hallo"))
+;; => (if pred then nil)
+
+
+;; Normal gequotet
+(println '(1 2 (+ 1 2) (+ 2 2)))
+
+;; syntax-gequotet mit unquote
+(println `(1 2 ~(+ 1 2) (+ 2 2)))
+
+;; my-when mit syntax-quoting
+(defmacro my-when-mit-syntax-quote
+  [pred then]
+  `(if ~pred
+     ~then
+     'nil))
+
+(macroexpand-1 '(my-when-mit-syntax-quote (= 1 1) "Hallo"))
+;; => (if (= 1 1) "Hallo" nil)
+
+(println "\n----Unquote-Splicing----\n")
+
+(let [thens (list 3 4 5)]
+  (println `(1 2 ~thens 6 7))   ; => (1 2 (3 4 5) 6 7)
+  (println `(1 2 ~@thens 6 7))) ; => (1 2 3 4 5 6 7)
+
+
+(defmacro my-when+ [pred & thens]
+  `(if ~pred
+     (do ~@thens)
+     nil))
+
+(macroexpand-1 '(my-when+ (= 1 1) (println "Hallo Hallo") 3))
+
+(println "\n----Makro-Hygiene----\n")
+
+;; Accidental variable capture
+(defmacro my-identity [a]
+  (list 'let ['x 0] a))
+
+;; Gensym
+(println "Ein neues Symbol" (gensym))
+(println "Ein neues Symbol mit Präfix" (gensym "blub"))
+
+
+;; Vermeintliche Identitätsfunktion
+(println "15 = " (my-identity 15) "?")
+(println "15 = "
+         (let [x 12] (my-identity x)) "?")
+
+
+
+
+
+;;; Code zum dritten Blogpost
+
+(println "\n====Blogpost 3====")
+
+;; Threading operator `my->>`
+
+(defmacro my->> [expr & args]
+  (if (empty? args)
+    expr
+    (let [[f & f-args] (first args)]
+      `(my->> (~f ~@f-args ~expr)
+              ~@(rest args)))))
+
+(my->> [1 2 3 4 5 6 7 8 9]
+       (map inc)
+       (remove #(= 0 (mod % 2)))
+       (reverse)
+       (rest))
+
+
+
+(rest (reverse (remove #(= 0 (mod % 2))
+                       (map inc
+                            [1 2 3 4 5 6 7 8 9]))))
+
+
+;; Threading operator, anaphorisch
+(defmacro as->> [expr name & args]
+  (if (empty? args)
+    expr
+    `(let [~name ~expr]
+       (as->> ~(first args) ~name ~@(rest args)))))
+
+(let [it 100]
+  (as->> 15 it
+         (+ it it)))
+
+(defmacro testi [f]
+  (let [it 'it]
+    `(let [~it 3]
+       ~f)))
+
+(let [it 100]
+  (testi (+ 1 it)))
+
+
+
+
+
 
 ;;; Das Record-Makro
 (println "\n----Das Record-Makro----\n")
